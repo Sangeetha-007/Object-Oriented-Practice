66946b5fb1ee6c84b6e9e6c6c94935d0207a387d
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2017-02-20 20:03:37 -0500

    Update DriverProgram.java

--- DriverProgram.java
+++ DriverProgram.java
@@ -1,11 +1,13 @@
 
 public class DriverProgram {
 	public static void main(String[] args){
+		
 		Queen lastQueen = null;
-		for(int i = 1; i <= 8; i++)
+		for(int i = 1; i <= 10; i++)  //originally it said i<=8
 		{
 			lastQueen = new Queen(i,lastQueen);
 		}
+		//System.out.print("No solution");
 		lastQueen.print();
 	}
 }
