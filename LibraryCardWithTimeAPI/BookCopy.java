d0a0b998fa2a2444fff0e0942b6f17877516fa15
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2017-02-27 19:09:02 -0500

    Update BookCopy.java

--- LibraryCardWithTimeAPI/BookCopy.java
+++ LibraryCardWithTimeAPI/BookCopy.java
@@ -1,17 +1,31 @@
+import java.time.*;
+import java.util.*;
+
 // Sample solution for HW1
 public class BookCopy {
 	private Book book;
 	private LibraryCard card;
+	private LocalDate DueDate;
 	
 	public BookCopy(Book b)
 	{
 		book = b;
 		card = null;
+		DueDate=null;
 	}
 	
 	public Book getBook() {return book;}
 	public LibraryCard getCard() {return card;}
 	
+
+    public LocalDate getDueDate() {
+        return DueDate;
+    }
+    public void setDueDate(LocalDate a) {
+          DueDate=a;
+    }
+    
+  
 	public boolean checkOut(LibraryCard borrower)
 	//checks book out by setting card reference to borrower.
 	//returns false if book is already checked out
@@ -19,15 +33,13 @@ public class BookCopy {
 		//DueDate weekLater = todaysDate.plusWeeks(3);
 		if (card != null)
 			return false;
+		DueDate=LocalDate.now().plusWeeks(3);  //now is a static method of the LocalDate class
 		card = borrower;
 		return true;
 	}
-	public boolean DueDate(    ){
-		
-		if(checkOut==false)
-			return null;
-		
-	}
+	
+	
+	
 	
 	public boolean returnBook()
 			//returns book by removing card reference
@@ -35,6 +47,13 @@ public class BookCopy {
 	{
 		if (card == null)
 			return false;
+		if( this.getDueDate().getYear() == LocalDate.now().getYear() )
+		else if (this.DueDate().getDayOfYear()< LocalDate.now().getDayOfYear() )
+			{
+				int numDays = LocalDate.now().getDayOfYear() - this.DueDate().getDayOfYear()+ LocalDate.now().getDayOfYear();
+				card.addToBalance(numDays * 0.10); 
+			}
+				
 		card = null;
 		return true;
 	}
